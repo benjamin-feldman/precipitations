eed9b1d1010dd0aaac104a42b04daf9e5edad1ca
Benjamin <benjamin.feldman666@gmail.com>
2024-04-20 16:43:47 +0200

    debug and optimize

--- src/make_dataset.py
+++ src/make_dataset.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 import numpy as np
-from utils import segmentation_events, event_length, GRID_SIZE, TIME_STEPS_PER_DAY
+from utils import GRID_SIZE, TIME_STEPS_PER_DAY, get_events, read_data, timer
 import argparse
 import re
 
@@ -15,8 +15,8 @@ class DatasetCreator:
         self.month_names = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
     
     @staticmethod
-    def get_date_from_file(file):
-        match = re.search(r'(\d{4})(\d{2})(\d{2})', file)
+    def get_date_from_file(filename):
+        match = re.search(r'(\d{4})(\d{2})(\d{2})', filename)
         return int(match.group(1)), int(match.group(2)), int(match.group(3))
     
     @staticmethod
@@ -28,7 +28,7 @@ class DatasetCreator:
     def extract_df_month(self):
         print(f"Listing all files for {self.year}-{self.month}.")
         files = os.listdir(f'data/raw_data/{self.year}')
-        dates = [self.get_date_from_file(file) for file in files if file.endswith('.npy')]
+        dates = [self.get_date_from_file(filename) for filename in files if filename.endswith('.npy')]
         dates = [date for date in dates if date[1] == self.month]
 
         print(f"Total files for {self.month}/{self.year}: {len(dates)}")
@@ -37,20 +37,24 @@ class DatasetCreator:
         count = 0
         for index, (year, month, day) in enumerate(dates):
             print(f"Processing file {index + 1}/{len(dates)}: {day}/{month}/{year}")
-            raw_events, raw_data = segmentation_events(year, month, day)
-            events = event_length(raw_events)
+            raw_data = read_data(year, month, day)
+            events = get_events(raw_data)
             for i in range(GRID_SIZE):
                 for j in range(GRID_SIZE):
-                    if count % 1000 == 0:
+                    if count % 10000 == 0:
                         print(f"{day}/{month}/{year}: {count} / {max_count}")
                     count += 1
-                    for event_index, (start_time, duration) in enumerate(events[i, j]):
+                    for event in events[i, j]:
+                        start_time, end_time = event
+                        duration = end_time - start_time + 1
                         stats = self.event_stats(raw_data, i, j, start_time, duration)
                         rows.append({
                             'year': year, 'month': month, 'day': day, 
                             'i': i, 'j': j, 
                             'start_time_relative': start_time, 
-                            'start_time_absolute': start_time + TIME_STEPS_PER_DAY * (day - 1), 
+                            'end_time_relative': end_time,
+                            'start_time_absolute': start_time + TIME_STEPS_PER_DAY * (day - 1),
+                            'end_time_absolute': end_time + TIME_STEPS_PER_DAY * (day - 1),
                             'duration': duration, 
                             'max_intensity': stats[1], 'mean_intensity': stats[2], 
                             'variance': stats[3], 'percentage_null': stats[4]
@@ -58,11 +62,15 @@ class DatasetCreator:
         print("Data extraction complete.")
         return pd.DataFrame(rows)
 
-
+    @timer
     def write_df_month(self):
         print(f"Extracting dataframe for {self.year}, {self.month_names[self.month-1]}")
         df = self.extract_df_month()
-        df['end_time_absolute'] = df['start_time_absolute'] + df['duration']
+        try:
+            df = df[df["max_intensity"] > 0] # workaround
+        except KeyError:
+            print(f"No rain in {self.month} of {self.year}")
+
         
         directory = 'data/dataframes'
         if not os.path.exists(directory):
@@ -73,11 +81,13 @@ class DatasetCreator:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Create dataset for a specific year and month, save to .pkl')
-    parser.add_argument('-y', '--year', default=2020, type=int, required=False, help='The year of the dataset.')
-    parser.add_argument('-s', '--start_month', default=6, type=int, required=False, help='The start month for which the dataset will be created.')
+    parser.add_argument('-y', '--year', default=2021, type=int, required=False, help='The year of the dataset.')
+    parser.add_argument('-s', '--start_month', default=1, type=int, required=False, help='The start month for which the dataset will be created.')
     parser.add_argument('-e', '--end_month', default=12, type=int, required=False, help='The end month for which the dataset will be created.')
     args = parser.parse_args()
 
     for month in range(args.start_month, args.end_month + 1):
         dc = DatasetCreator(args.year, month)
         dc.write_df_month()
+    
+    # python src/make_dataset.py --year 2017
--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,11 @@
 from joblib import Parallel, delayed
 import numpy as np
+import time
 
 GRID_SIZE = 300
 TIME_STEPS_PER_DAY = 288
 
-def read_data(year, month, day):
+def read_data(year:int, month:int, day:int) -> np.ndarray:
     path = f'data/raw_data/{year:04d}/'
     file_name = f'RR_IDF300x300_{year:04d}{month:02d}{day:02d}.npy'
     full_path = path + file_name
@@ -15,8 +16,63 @@ def read_data(year, month, day):
         raise FileNotFoundError(f"The file {full_path} does not exist.")
     return raw_data
 
+def segment_events(time_series: np.ndarray) -> list:
+    events = []
+    current_event_start = None
 
-def segmentation_events(year, month, day):
+    for i in range(len(time_series)):
+        current_value = time_series[i]
+        if current_value != 0 and not np.isnan(current_value):
+            if current_event_start is None:
+                # Start a new event if there's none active
+                current_event_start = i
+            elif i > 0 and (i - last_non_zero_index) > 5:
+                # If current index and last non-zero index are more than 5 minutes apart, end current event
+                events.append((current_event_start, last_non_zero_index))
+                current_event_start = i  # Start a new event
+        else:
+            if current_event_start is not None and (i - last_non_zero_index) > 5:
+                # End current event if there's a gap of more than 30 minutes
+                events.append((current_event_start, last_non_zero_index))
+                current_event_start = None
+        
+        if current_value != 0 and not np.isnan(current_value):
+            last_non_zero_index = i  # Update the last non-zero index
+
+    # If an event is still open at the end of the array, close it
+    if current_event_start is not None:
+        events.append((current_event_start, last_non_zero_index))
+
+    return events
+
+def get_events(raw_data: np.ndarray) -> np.ndarray: #raw_data coming from read_data(year, month, day)
+    global GRID_SIZE
+
+    events = np.empty((GRID_SIZE, GRID_SIZE), dtype=object)
+    for i in range(GRID_SIZE):
+        for j in range(GRID_SIZE):
+            pixel_events = segment_events(raw_data[:, i, j])
+            events[i, j] = pixel_events
+            
+    return events
+
+def timer(func):
+    """
+    Decorator that prints the execution time of the function it decorates.
+    """
+    def wrapper(*args, **kwargs):
+        start_time = time.time()  # Record the start time
+        result = func(*args, **kwargs)  # Call the function
+        end_time = time.time()  # Record the end time
+        print(f"{func.__name__} executed in {end_time - start_time:.4f} seconds")
+        return result
+    return wrapper
+
+# Legacy code
+
+def segmentation_events_legacy(year, month, day):
+    global TIME_STEPS_PER_DAY, GRID_SIZE
+
     raw_data = read_data(year, month, day)
     raw_events = np.zeros((TIME_STEPS_PER_DAY, GRID_SIZE, GRID_SIZE))
 
@@ -35,10 +91,11 @@ def segmentation_events(year, month, day):
                 t += 1
     return raw_events, raw_data
 
-def event_length(raw_events):
+def event_length_legacy(raw_events):
     """
     Calculates the length of weather events.
     """
+    global TIME_STEPS_PER_DAY, GRID_SIZE
     lengths = np.empty((GRID_SIZE, GRID_SIZE), dtype=object)
 
     for i in range(GRID_SIZE):
@@ -55,9 +112,3 @@ def event_length(raw_events):
                     t += 1
             lengths[i, j] = events
     return lengths
-
-def extract_time_series(year, month, day, pixels_indices: list):
-    raw_data = read_data(year, month, day)
-    res = []
-    for i, j in pixels_indices:
-        res.append(raw_data[:, i, j])
